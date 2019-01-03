6f05af023e904b709098d089ba841cd5c1a0c25d
wilsonify <tom.andrew.wilson@gmail.com>
2019-01-02 18:11:08 -0600

    lint

--- swagger_server/controllers/predict_controller.py
+++ swagger_server/controllers/predict_controller.py
@@ -9,12 +9,12 @@ import six
 from swagger_server.models.input import Input  # noqa: E501
 from swagger_server import util
 
-
 abspath_current = os.path.abspath(__file__)
 dname_current = os.path.dirname(abspath_current)
 pickle_path = os.path.join(dname_current, "house_price_model")
 with open(pickle_path, 'rb') as handle:
     house_price_model = (pickle.load(handle))
+
 
 def predict_post(body):  # noqa: E501
     """make a prediction
@@ -27,11 +27,10 @@ def predict_post(body):  # noqa: E501
     :rtype: None
     """
 
-
     if connexion.request.is_json:
         body = connexion.request.get_json()  # noqa: E501
 
-    df = pd.DataFrame(body,index=range(len(body)-1))
+    df = pd.DataFrame(body, index=range(len(body) - 1))
     pred = house_price_model.predict(df[['age']])
     pred = pd.DataFrame(pred, columns=['predicted_price'])
     pred = pd.concat([df, pred], axis=1)
