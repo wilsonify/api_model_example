b9505790c6550e3033bd9b311fe334d61d47a483
wilsonify <tom.andrew.wilson@gmail.com>
2019-01-02 18:09:54 -0600

    move pickle back to controller

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -7,13 +7,6 @@ from swagger_server import encoder
 
 
 def main():
-    global house_price_model
-    abspath_current = os.path.abspath(__file__)
-    dname_current = os.path.dirname(abspath_current)
-    pickle_path = os.path.join(dname_current, "house_price_model")
-    with open(pickle_path, 'rb') as handle:
-        house_price_model = (pickle.load(handle))
-
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Boston Housing Price'})
--- swagger_server/controllers/predict_controller.py
+++ swagger_server/controllers/predict_controller.py
@@ -10,6 +10,12 @@ from swagger_server.models.input import Input  # noqa: E501
 from swagger_server import util
 
 
+abspath_current = os.path.abspath(__file__)
+dname_current = os.path.dirname(abspath_current)
+pickle_path = os.path.join(dname_current, "house_price_model")
+with open(pickle_path, 'rb') as handle:
+    house_price_model = (pickle.load(handle))
+
 def predict_post(body):  # noqa: E501
     """make a prediction
 
@@ -25,7 +31,7 @@ def predict_post(body):  # noqa: E501
     if connexion.request.is_json:
         body = connexion.request.get_json()  # noqa: E501
 
-    df = pd.DataFrame.from_records(body)
+    df = pd.DataFrame(body,index=range(len(body)-1))
     pred = house_price_model.predict(df[['age']])
     pred = pd.DataFrame(pred, columns=['predicted_price'])
     pred = pd.concat([df, pred], axis=1)
